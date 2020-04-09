5e61fd0898a5122463ebb6f94e4872d3274451aa
Anggara1234 <anggaramag@gmail.com>
2020-04-09 11:47:36 +0700

    Updates

---
+++ precache-manifest.74e745fb28d4ad9cf4e0256acc6200a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f806f92863de903116235ad80bc221",
+    "url": "/TDS_Frontend-Developer/index.html"
+  },
+  {
+    "revision": "a4d965807597bcce02d3",
+    "url": "/TDS_Frontend-Developer/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8a9cccd610c53bd99b9e",
+    "url": "/TDS_Frontend-Developer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4d965807597bcce02d3",
+    "url": "/TDS_Frontend-Developer/static/js/2.1b3f4392.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TDS_Frontend-Developer/static/js/2.1b3f4392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9cccd610c53bd99b9e",
+    "url": "/TDS_Frontend-Developer/static/js/main.66d715f6.chunk.js"
+  },
+  {
+    "revision": "0686f1cc7d82ef46714a",
+    "url": "/TDS_Frontend-Developer/static/js/runtime-main.3f4e0e9b.js"
+  }
+]);
